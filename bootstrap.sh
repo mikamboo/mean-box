74d4d67d98e42a94baf14bedf1b6fe482ee2292c
Michaël P.O <mikamboo@gmail.com>
2015-02-06 13:40:34 +0000

    create mean install script

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+############## MEAN STACK INSTALLATION ################
+
+# Install git 
+sudo apt-get update --yes
+sudo apt-get -y install git
+
+# Install nodejs (include npm)
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update --yes
+
+#-- Install nodejs & npm
+sudo apt-get install -y nodejs
+
+#Install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update --yes
+sudo apt-get install -y mongodb-org
+
+# Install Node.js tools
+sudo npm install -g bower grunt-cli yo generator-meanjs
+
+# TESTS installed packages
+git --version && node -v && npm -v && mongo -version
+
